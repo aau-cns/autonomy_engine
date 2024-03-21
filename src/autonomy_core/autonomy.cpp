3aa5555a55349a2229302888f7643526b7fe3c3a
Fornasier, Alessandro <alessandro.fornasier@aau.at>
2024-03-21 08:19:58 +0100

    [MAINT] Removed check on recursive call to mission selection (tested on alf machine)

--- src/autonomy_core/autonomy.cpp
+++ src/autonomy_core/autonomy.cpp
@@ -1330,12 +1330,8 @@ void Autonomy::missionSelection()
   {
     logger_.logUI(state_->getStringFromState(), ESCAPE(BOLD_ESCAPE, RED_ESCAPE), "\n >>> Wrong mission ID chosen\n\n");
 
-    // HACK(scm): CTRL+C creates mission_id_ = -1, so only continue if mission_id_ is not -1
-    if (mission_id_ != -1)
-    {
-      // Call recursively mission selection
-      missionSelection();
-    }
+    // Call recursively mission selection
+    missionSelection();
   }
   else
   {
